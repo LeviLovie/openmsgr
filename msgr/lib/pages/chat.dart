036b3ef9e8f80e0217333acf8527d7f38f2240cb
Leo <levi@Leos-MacBook-Air-2.local>
2025-08-05 14:26:31 +0300

    Update the chat page

--- msgr/lib/pages/chat.dart
+++ msgr/lib/pages/chat.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class ChatPage extends StatelessWidget {
+import '/pages/profile.dart';
+
+final supabase = Supabase.instance.client;
+
+class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
 
   @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  @override
+  void initState() {
+    _setupAuthListener();
+    super.initState();
+  }
+
+  void _setupAuthListener() {
+    supabase.auth.onAuthStateChange.listen((data) {
+      final event = data.event;
+      switch (event) {
+        case AuthChangeEvent.signedOut:
+          Navigator.pushReplacementNamed(context, '/login');
+        default:
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Text.rich(TextSpan(text: 'Chat'));
+    return Scaffold(
+      appBar: AppBar(title: const Text('Chat')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(children: <Widget>[Text("Chat")]),
+      ),
+    );
   }
 }
